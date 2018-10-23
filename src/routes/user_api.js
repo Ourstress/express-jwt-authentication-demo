98aa840d4c1e7c1c8eb0dddabb80181a82d666de
SG <sgng1@e.ntu.edu.sg>
2018-10-23 09:10:43 +0100

    added authentication tests

--- src/routes/user_api.js
+++ src/routes/user_api.js
@@ -4,5 +4,6 @@ const handleAsyncError = require("express-async-wrap");
 const userHandler = require("../handler/user_handler");
 
 router.post("/signup", handleAsyncError(userHandler.registerNewUser));
+router.post("/login", handleAsyncError(userHandler.login));
 
 module.exports = router;
